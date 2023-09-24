0dff2d28dfd0f4d6187a1e3c1a95db992fe2958e
jarviik <136592352+jarviik@users.noreply.github.com>
2023-09-24 19:28:15 +0300

    Delete starter/shopping-lists directory

--- starter/shopping-lists/deps.js
+++
@@ -1,4 +0,0 @@
-export { serve } from "https://deno.land/std@0.171.0/http/server.ts";
-export { Pool } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
-export { renderFile } from "https://deno.land/x/eta@v1.12.3/mod.ts";
-export { configure } from "https://deno.land/x/eta@v1.12.3/mod.ts";
