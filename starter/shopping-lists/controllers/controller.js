ce6d42a257430da71734f19bbc3026315058d410
jarviik <136592352+jarviik@users.noreply.github.com>
2023-09-24 18:45:05 +0300

    Update controller.js

--- starter/shopping-lists/controllers/controller.js
+++ starter/shopping-lists/controllers/controller.js
@@ -1,6 +1,6 @@
 import { renderFile } from "https://deno.land/x/eta@v1.12.3/mod.ts";
-import * as listService from "../services/listService.js";
-import * as itemService from "../services/itemService.js";
+import * as listService from "../services/listsService.js";
+import * as itemService from "../services/itemsService.js";
 
 const responseDetails = {
     headers: { "Content-Type": "text/html;charset=UTF-8" },
